bc7308bf09020a6694ad6d6bfb1f2dfe70c80c35
blueC4P <osamag4g@gmail.com>
2021-06-01 22:44:24 +0100

    Improve HeroCard and route to HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom';
+
+import { getHeroesById } from '../../selectors/getHeroesById';
 
 export const HeroScreen = () => {
+
+    const {heroId} = useParams();
+    console.log(heroId)
+    
+    const hero = getHeroesById(heroId);
+
+    if(!hero){
+        return <Redirect to="/"/>
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
     return (
         <div>
             <h1>Hero Screen</h1>
