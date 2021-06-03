3f00d539196b761ac1cbe8d859d8acba2f315693
blueC4P <osamag4g@gmail.com>
2021-06-03 16:55:22 +0100

    HeroScreen Improved and add animations to HeroCards and HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,30 +1,56 @@
-import React from 'react'
-import { Redirect, useParams } from 'react-router-dom';
+import React, { useMemo } from "react";
+import { Redirect, useParams } from "react-router-dom";
 
-import { getHeroesById } from '../../selectors/getHeroesById';
+import { getHeroesById } from "../../selectors/getHeroesById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
+  const { heroId } = useParams();
 
-    const {heroId} = useParams();
-    console.log(heroId)
-    
-    const hero = getHeroesById(heroId);
+  const hero = useMemo(() => getHeroesById(heroId), [heroId])
 
-    if(!hero){
-        return <Redirect to="/"/>
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push("/");
+    } else {
+      history.goBack();
     }
+  };
 
-    const {
-        superhero,
-        publisher,
-        alter_ego,
-        first_appearance,
-        characters,
-    } = hero;
+  const { superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
 
-    return (
-        <div>
-            <h1>Hero Screen</h1>
-        </div>
-    )
-}
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={`Imagen de ${superhero}`}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: </b> {first_appearance}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
+    </div>
+  );
+};
